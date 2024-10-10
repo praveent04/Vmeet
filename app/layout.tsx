2e9703f56bcebfd439d26d04d9301e5c57eb0fec
Praveen Tiwari <praveentiwari29004@gmail.com>
2024-10-10 18:20:42 +0530

    error -> cannot destructure property camera of call as it is undefined

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Toaster } from "@/components/ui/toaster"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -46,6 +46,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable}  bg-dark-2`}
       >
         {children}
+        <Toaster/>
       </body></ClerkProvider>
       
     </html>
